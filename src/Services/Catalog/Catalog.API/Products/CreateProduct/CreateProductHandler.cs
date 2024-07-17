e2b85da4ab8c1019623e5a002fe6b7bdb1d3d4c8
Praveen Rondla <pkrondla@gmail.com>
2024-07-17 14:44:18 +0530

    Catalog and Building Projects

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,8 @@
+﻿namespace Catalog.API.Products.CreateProduct;
+
+public record CreateProductCommand(string Name, List<string> Category,string Description,string ImageFile,decimal Price);
+public record CreateProductResult(Guid Id);
+internal class CreateProductCommandHandler
+{
+}
+
